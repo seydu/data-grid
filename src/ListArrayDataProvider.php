71e564c2218a580a8179c7723c2895f8dfbd4e29
Saidou Gueye <saidou.gueye@gmail.com>
2024-10-21 13:33:44 +0000

    Add ListArrayDataProvider class

---
+++ src/ListArrayDataProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Seydu\DataGrid;
+
+use Seydu\DataProvider\ListArrayDataProvider as BaseListArrayDataProvider;
+
+class ListArrayDataProvider extends BaseListArrayDataProvider
+{
+
+}
